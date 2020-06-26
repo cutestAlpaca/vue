61a476caed50ef7cf987589cda9cc9afcbb8543e
koko <alpaca5541@foxmail.com>
2020-06-26 20:02:53 +0800

    添加 登陆注册页面并使用validator验证 数据

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import Vue from "vue"
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue"
-import App from "./App.vue"
-import router from "./router"
-import store from "./store"
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import Vuelidate from 'vuelidate'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 // scss
-import "./assets/scss/index.scss"
+import './assets/scss/index.scss'
 
 Vue.config.productionTip = false
 
@@ -12,9 +13,10 @@ Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
+Vue.use(Vuelidate)
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount("#app")
+  render: (h) => h(App)
+}).$mount('#app')
